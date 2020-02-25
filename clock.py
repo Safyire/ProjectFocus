6acf78328d131c867e06eb2b10788f1e7551b948
Nathan Piedad <safyire.n8@gmail.com>
2020-02-25 14:22:04 -0800

    Implemented 12-hour clock

--- clock.py
+++ clock.py
@@ -9,7 +9,7 @@ pygame.init()
 size = [480, 320]
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("YOOOOOOO")
+pygame.display.set_mode(size, pygame.NOFRAME)
 
 done = False
 
@@ -21,12 +21,22 @@ while not done:
     screen.fill(BLACK)
     
     now = datetime.now()
-    current_time = now.strftime("%H:%M")
+    time_hour = now.strftime("%H")
+    time_hour_2 = int(time_hour)
+    time_minute = now.strftime("%M")
+    
+    if (time_hour_2 > 12):
+        time_hour_2 -= 12
+        time_hour = ("0" + str(time_hour_2))
+        
+    
+    
+    current_time = time_hour + ":" + time_minute
     
     #draw start
     font = pygame.font.SysFont('Calibri', 275, True, False)
     text = font.render(current_time, True, WHITE)
-    screen.blit(text, [0, 50])
+    screen.blit(text, [7, 50])
     #draw end
     
     pygame.display.flip()
