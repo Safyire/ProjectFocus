7c6740b16a5bacbecd334044d9a6c2e9f1a621d0
Nathan Piedad <safyire.n8@gmail.com>
2020-02-27 13:55:54 -0800

    Added template file and WIP game

---
+++ spinningline.py
@@ -0,0 +1,22 @@
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+pygame.init()
+
+size = [480, 320] #RPi screen size
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_mode(size, pygame.NOFRAME)
+
+done = False
+
+while not done:
+    #initialization
+    
+    #draw start
+    
+    #draw end
+    
+pygame.exit()
