a1e03c8fecaaa054550cbcd4b4d6e13fd17290c5
Nathan Piedad <safyire.n8@gmail.com>
2020-03-02 13:39:26 -0800

    idek what I even changed at this point

--- spinningline.py
+++ spinningline.py
@@ -18,23 +18,22 @@ done = False
 angle = 0
 
 while not done:
-    #initialization
     dimensions = [110, 30, 250, 250]
-    
+        
     #draw start
     pygame.draw.ellipse(screen, GREEN, dimensions, 2)
-    #pygame.draw.rect(screen, BLACK, dimensions, 2)
+    
+    angle += .1
     
     x = 125 * math.sin(angle) + 235
     y = 125 * math.cos(angle) + 155
-    
+                
     pygame.draw.line(screen, GREEN, [235, 155], [x, y], 2)
-    
-    angle = angle + .1
-    
+        
+            
     if angle > 2 * PI:
         angle = angle - 2 * PI
-        
+                
     pygame.display.flip()
     pygame.time.Clock().tick(60)
     pygame.draw.line(screen, BLACK, [235, 155], [x, y], 2)
