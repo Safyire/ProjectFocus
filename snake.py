bde9b7f971d95dddedf0cea3050f8d5bc00402e9
Anna Hoang <anna@hoang.io>
2020-03-03 14:24:51 -0800

    Worked on Cube and Snake classes.

--- snake.py
+++ snake.py
@@ -7,57 +7,70 @@ from tkinter import messagebox
 class cube(object):
     rows = 20
     w = 480
-    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
-        pass
+    def __init__(self, start, dirx, diry, color=(255,0,0)):
+        self.pos = start
+        self.dirx = 1
+        self.diry = 0
+        self.color = color
         
-    def move(self, dirnx, dirny):
-        pass
+    def move(self, dirx, diry):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)  # change position     
     
     def draw(self, surface, eyes=False):
-        pass
-        
+        dis = self.w // self.rows  # Width/Height of each cube
+        i = self.pos[0] # Current row
+        j = self.pos[1] # Current Column
 
+        pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
+        
+        if eyes: # Draws the eyes
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
+            
 class snake(object):
     body = []
     turns = {}
     def __init__(self, color, pos):
         self.color = color
-        self.head = cube(pos)  # The head will be the front of the snake
-        self.body.append(self.head)  # We will add head (which is a cube object)
+        self.head = cube(pos)  #Head will be the front of the snake
+        self.body.append(self.head)  #Adds head (which is a cube object)
         # to our body list
 
         # These will represent the direction our snake is moving
-        self.dirnx = 0 
-        self.dirny = 1
+        self.dirx = 0 
+        self.diry = 1
 
     def move(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # Check if user hit the red x
+            if event.type == pygame.QUIT:
                 pygame.quit()
-
-            keys = pygame.key.get_pressed()  # See which keys are being pressed
-
-            for key in keys:  # Loop through all the keys
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                elif keys[pygame.K_RIGHT]:
-                    self.dirnx = 1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                elif keys[pygame.K_UP]:
-                    self.dirnx = 0
-                    self.dirny = -1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                elif keys[pygame.K_DOWN]:
-                    self.dirnx = 0
-                    self.dirny = 1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        pos = pygame.mouse.get_pos()
+        dirx = pos[0]
+        diry = pos[1]
         
+        self.snake.dirx = dirx
+        self.snake.diry = diry
+        
+        for i, c in enumerate(self.body):  # Loop through every cube in body
+            p = c.pos[:]  #Stores the cubes position on the grid
+            if p in self.turns:  # If the cubes current position is one where turned
+                turn = self.turns[p]  # Get the direction should turn
+                c.move(turn[0],turn[1])  # Move cube in that direction
+                if i == len(self.body)-1:  # If this is the last cube in body remove the turn from the dict
+                    self.turns.pop(p)
+            else: 
+                # If the cube reaches the edge of the screen we will make it appear on the opposite side
+                if c.dirx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.diry == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.diry == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                else: c.move(c.dirx,c.diry)  # If we haven't reached the edge just move in current direction
 
     def reset(self, pos):
         pass
@@ -67,8 +80,12 @@ class snake(object):
         
 
     def draw(self, surface):
-        pass
-
+        for i, c in enumerate(self.body):
+            if i == 0:  
+                c.draw(surface, True)  # adding the true as an argument will tell us to draw eyes
+            else:
+                c.draw(surface)  # otherwise we will just draw a cube 
+        
 
 def drawGrid(w, rows, surface):
     sizeBtwn = w // rows  # Gives us the distance between the lines
@@ -84,7 +101,9 @@ def drawGrid(w, rows, surface):
         
 
 def redrawWindow(surface):
+    global rows, width, s
     surface.fill((0,0,0))  # Fills the screen with black
+    s.draw(surface)
     drawGrid(surface)  # Will draw our grid lines
     pygame.display.update()  # Updates the screen
 
@@ -105,6 +124,8 @@ def main():
 
     win = pygame.display.set_mode((width, height), pygame.NOFRAME)  # Creates our screen object
 
+    pygame.mouse.set_visible(False)
+    
     s = snake((255,0,0), (10,10))  # Creates a snake object which we will code later
   
     clock = pygame.time.Clock() # creating a clock object
@@ -114,5 +135,6 @@ def main():
     while flag:
         pygame.time.delay(50)  # This will delay the game so it doesn't run too quickly
         clock.tick(10)  # Will ensure our game runs at 10 FPS
+        s.move()
         redrawWindow(win)  # This will refresh our screen  
 main()
