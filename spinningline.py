7a9c5bb77070cdf4857f3d89f8e0190eaf2c70ea
Nathan Piedad <safyire.n8@gmail.com>
2020-03-03 14:14:43 -0800

    Partial implementation of mouse + keyboard input

--- spinningline.py
+++ spinningline.py
@@ -13,13 +13,14 @@ size = [480, 320] #RPi screen size
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_mode(size, pygame.NOFRAME)
-
 done = False
 angle = 0
+spinning = True #Contribution by Crockett
+event = pygame.key.get_pressed()
 
 while not done:
     dimensions = [110, 30, 250, 250]
-        
+    
     #draw start
     pygame.draw.ellipse(screen, GREEN, dimensions, 2)
     
@@ -27,13 +28,15 @@ while not done:
     
     x = 125 * math.sin(angle) + 235
     y = 125 * math.cos(angle) + 155
-                
+    
     pygame.draw.line(screen, GREEN, [235, 155], [x, y], 2)
-        
-            
+    
     if angle > 2 * PI:
         angle = angle - 2 * PI
-                
+    
+    if event[pygame.K_SPACE]:
+        done = True
+        
     pygame.display.flip()
     pygame.time.Clock().tick(60)
     pygame.draw.line(screen, BLACK, [235, 155], [x, y], 2)
