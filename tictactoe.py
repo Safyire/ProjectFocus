6ef53b345a8a6cbfe41230df35070383acc92640
Nathan Piedad <safyire.n8@gmail.com>
2020-03-06 14:10:52 -0800

    Added field

--- tictactoe.py
+++ tictactoe.py
@@ -5,7 +5,7 @@ WHITE = (255, 255, 255)
 
 pygame.init()
 
-size = [480, 320] #RPi screen size
+size = [480, 320]  # RPi screen size
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_mode(size, pygame.NOFRAME)
@@ -13,18 +13,26 @@ pygame.display.set_mode(size, pygame.NOFRAME)
 done = False
 
 while not done:
-    #initialization
-    dimensions = [110, 35, 250, 250]
+    # initialization
+    x_base = 110
+    y_base = 35
+    main_dimensions = [110, 35, 250, 250] # [x, y, h, w]
     
-    #draw start
-    pygame.draw.rect(screen, WHITE, dimensions, 3)
+    # draw start
+    pygame.draw.rect(screen, WHITE, main_dimensions, 3)
     
-    #Exit if the mouse is clicked on the window (tap)
+    pygame.draw.line(screen, WHITE, [x_base*1.755, y_base], [x_base*1.755, 320-y_base], 3)
+    pygame.draw.line(screen, WHITE, [x_base*2.5, y_base], [x_base*2.5, 320-y_base], 3)
+    
+    pygame.draw.line(screen, WHITE, [x_base, y_base+(250/3)], [x_base*3.25, y_base+(250/3)], 3)
+    pygame.draw.line(screen, WHITE, [x_base, y_base+(500/3)], [x_base*3.25, y_base+(500/3)], 3)
+    
+    # (For testing) Exit if the mouse is clicked on the window (tap)
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             done = True
             
-    #draw end
+    # draw end
     pygame.display.flip()
     
 pygame.quit()
